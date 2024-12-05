864331209d03d457f36deeae7c50f4c08cb3bcaf
Ubuntu <ubuntu@ip-172-31-4-79.ec2.internal>
2024-12-05 13:04:55 +0000

    alterado endereço para internet.com

--- src/main/java/br/ufs/dcomp/InetAddressTest/App.java
+++ src/main/java/br/ufs/dcomp/InetAddressTest/App.java
@@ -12,7 +12,7 @@ public class App
     {
         try{
             
-            String name = "ufs.br";
+            String name = "internet.com";
             InetAddress address = InetAddress.getByName(name); 
             System.out.println( "Name:      "+ name);
             System.out.println( "Address:   "+ address.getHostAddress());
